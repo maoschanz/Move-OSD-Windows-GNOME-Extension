3cd81e496cdc9500992d37e7a58b87d78a0c7d81
Romain F. T <rrroschan@gmail.com>
2021-07-11 18:41:33 +0200

    script to update the translations

---
+++ translations.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+################################################################################
+
+TRANSLATION_ID="move-osd-windows"
+
+echo "Generating .pot file..."
+xgettext --files-from=POTFILES --from-code=UTF-8 --add-location=file --output=$TRANSLATION_ID@maestroschan.fr/locale/$TRANSLATION_ID.pot
+
+################################################################################
+
+IFS='
+'
+dir_list=`ls ./$TRANSLATION_ID@maestroschan.fr/locale/`
+prefix="./$TRANSLATION_ID@maestroschan.fr/locale"
+
+for dir in $dir_list; do
+	if [ "$dir" != "$TRANSLATION_ID.pot" ]; then
+		echo "Updating translation for: $dir"
+		msgmerge --update --previous $prefix/$dir/LC_MESSAGES/$TRANSLATION_ID.po $prefix/$TRANSLATION_ID.pot
+		echo "Compiling translation for: $dir"
+		msgfmt $prefix/$dir/LC_MESSAGES/$TRANSLATION_ID.po -o $prefix/$dir/LC_MESSAGES/$TRANSLATION_ID.mo
+		rm -f "$prefix/$dir/LC_MESSAGES/$TRANSLATION_ID.po~"
+	fi
+done
+
+################################################################################
+
+exit 0
+
